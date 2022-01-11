520b2b0c1bbacfab92e4cda77b9a91cd3c97b7c8
Burak Duran <burak.duraninfo@gmail.com>
2022-01-11 07:38:08 +0300

    First day completed

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof(userName))
+console.log(typeof(isHuman))
+console.log(typeof(tanimsiz))
+console.log(typeof(bos))
---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,30 @@
+
+let userName = "Vlex" // String
+const isHuman = true // Boolean
+let tanimsiz = undefined // Undefined
+let bos = null // Object
+
+let mouse;
+let keyboard;
+let monitor;
+let camera;
+
+mouse = "Logitech"
+keyboard = "Logitech"
+monitor = "MSI"
+camera = "none"
+
+console.log(mouse, keyboard, monitor, camera)
+
+//first name, last name, marital status, country and age
+
+let firstName = "Burak"
+lastName = "Duran"
+martialStatus = "Single"
+country = "Turkey"
+age = 21;
+
+console.log(firstName, lastName, martialStatus, country, age)
+
+let myAge = 21
+let yourAge = 30
